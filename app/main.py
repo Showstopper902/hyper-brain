043caef3523c964287ea7a65d8321902bbeca994
Showstopper902 <yougotted@yougotinvested.com>
2026-01-31 20:29:03 -0800

    v6

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 import os
 from typing import Any, Dict, Optional
+from collections.abc import Mapping
 
 from fastapi import Depends, FastAPI, HTTPException, Request
 from pydantic import BaseModel
